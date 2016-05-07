6a50c2922dfa8d565cb3fc146fde596a7a85af84
mnorelli <mnorelli360@gmail.com>
2016-05-07 15:06:59 -0700

    add pseudocode, playspace refactor, divs for grid lines

--- game/scripts/gameplay.js
+++ game/scripts/gameplay.js
@@ -1,5 +1,38 @@
 window.onload = function() {
 
+Pseudo code:
+
+// SETUP
+
+// Add background
+// Add titles
+// Draw grid
+// Add event listeners
+// Add reset button
+// Message: start, X goes first
+
+// PLAY
+// When grid is clicked:
+//     add letter
+//     update game logic array
+//     toggle X/O
+//     lock cell
+// Look for winnner
+// If winner, run winner function
+
+// WINNER
+// Change Message
+// Remove grid lines?
+// Flash winning sequence?
+// Message: restart
+
+// GAME LOGIC
+// Create array of locations alongside CSS
+// function: test for one letter
+//   if three-in-row return winner
+
+
+
 console.log("Linked!");
 
 };
