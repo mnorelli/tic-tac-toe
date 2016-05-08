146d7c827ede8442a6b2e7bc9cdb5630ce4889dd
mnorelli <mnorelli360@gmail.com>
2016-05-07 18:50:28 -0700

    add div boxes and text for grid cells, change to background image

--- game/scripts/gameplay.js
+++ game/scripts/gameplay.js
@@ -1,37 +1,47 @@
 window.onload = function() {
 
-//Pseudo code:
+/*
 
-// SETUP
+Pseudo code:
 
-// Add background
-// Add titles
-// Draw grid
-// Add event listeners
-// Add reset button
-// Message: start, X goes first
+SETUP
 
-// PLAY
-// When grid is clicked:
-//     add letter
-//     update game logic array
-//     toggle X/O
-//     lock cell
-// Look for winnner
-// If winner, run winner function
+  Add background
+  Add titles
+  Draw grid
+  Add event listeners
+  Add reset button
+  Message: start, X goes first
 
-// WINNER
-// Change Message
-// Remove grid lines?
-// Flash winning sequence?
-// Message: restart
+PLAY
+  When grid is clicked:
+      add letter
+      update game logic array
+      toggle X/O
+      lock cell
+  Look for winnner
+  If winner, run winner function
 
-// GAME LOGIC
-// Create array of locations alongside CSS
-// function: test for one letter
-//   if three-in-row return winner
+WINNER
+  Change Message
+  Remove grid lines?
+  Flash winning sequence?
+  Message: restart
 
+GAME LOGIC
+  Create array of locations alongside CSS
+  function: test for one letter
+    if three-in-row return winner
 
+GRID SETUP
+
+  a | b | c
+  -- --- --
+  d | e | f
+  -- --- --
+  g | h | i
+
+*/
 
 console.log("Linked!");
 
