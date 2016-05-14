b4db071559c53d6358b05d5bcccb6b0fe581a748
mnorelli <mnorelli360@gmail.com>
2016-05-14 11:30:32 -0700

    move player noise into markCell function and increase silence at start of clap sound, so that player noise is heard after winning or tie play

--- game/scripts/gameplay.js
+++ game/scripts/gameplay.js
@@ -72,6 +72,8 @@ function markCell(e) {
     positions[cell.id] = player;              // update the logic array
     cell.style.pointerEvents = 'none';  // turn off further cell clicks
   }
+  if (player==='X') x_sound.play();
+    else o_sound.play();
   if (winner()) {endGame()}
     else if (tie()) {tieGame()}
     else {alternatePlayer()}                 // switch the player
@@ -82,11 +84,9 @@ function alternatePlayer() {
   if (player==='X') {
     player='O';
     playerColor = "orange";
-    x_sound.play();
   } else {
     player='X';
     playerColor = "lightgreen";
-    o_sound.play();
     }
   say(player+"'s turn...",playerColor); 
 }
